bfbc57f6c99b8c2df5b2b027574447bb719b78b8
Maria <79389256+marythedev@users.noreply.github.com>
2023-10-07 14:25:55 -0400

    added navbar for ultra small screens & added logo to all pages

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+document.querySelector('#ultra-small-screen .language-dropdown').onchange = function () {
+    window.location.href = this.value;
+}
+
 document.querySelector('#small-screen .language-dropdown').onchange = function () {
     window.location.href = this.value;
 }
@@ -7,7 +11,7 @@ document.querySelector('#large-screen .language-dropdown').onchange = function (
 }
 
 var navExpanded = false;
-document.getElementById('menu-button').addEventListener('click', () => {
+document.getElementById('menu-button-small').addEventListener('click', () => {
     if (navExpanded) {
         document.querySelector('#small-screen #nav-links').style.display = "none";
         document.getElementById('line1').classList.remove("nav-expanded");
@@ -17,6 +21,23 @@ document.getElementById('menu-button').addEventListener('click', () => {
     }
     else {
         document.querySelector('#small-screen #nav-links').style.display = "block";
+        document.getElementById('line1').classList.add("nav-expanded");
+        document.getElementById('line2').classList.add("nav-expanded");
+        document.getElementById('line3').classList.add("nav-expanded");
+        navExpanded = true;
+    }
+})
+
+document.getElementById('menu-button-ultra-small').addEventListener('click', () => {
+    if (navExpanded) {
+        document.querySelector('#ultra-small-screen #nav-links').style.display = "none";
+        document.getElementById('line1').classList.remove("nav-expanded");
+        document.getElementById('line2').classList.remove("nav-expanded");
+        document.getElementById('line3').classList.remove("nav-expanded");
+        navExpanded = false;
+    }
+    else {
+        document.querySelector('#ultra-small-screen #nav-links').style.display = "block";
         document.getElementById('line1').classList.add("nav-expanded");
         document.getElementById('line2').classList.add("nav-expanded");
         document.getElementById('line3').classList.add("nav-expanded");
