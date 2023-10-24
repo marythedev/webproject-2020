bcd1621e08eb3ab36d488ed40f0c24055834a7de
Maria <79389256+marythedev@users.noreply.github.com>
2023-10-24 19:12:26 -0400

    improved image responsiveness

--- script.js
+++ script.js
@@ -44,3 +44,22 @@ document.getElementById('menu-button-ultra-small').addEventListener('click', () 
         navExpanded = true;
     }
 })
+
+let zoomIn = document.getElementsByClassName('zoomed-out');
+for (let i = 0; i < zoomIn.length; i++) {
+    let img = zoomIn[i];
+    let zoomed = false;
+
+    img.addEventListener('click', () => {
+        if (zoomed) {
+            img.classList.remove('zoomed-in');
+            img.classList.add('zoomed-out');
+            zoomed = false;
+        }
+        else {
+            img.classList.remove('zoomed-out');
+            img.classList.add('zoomed-in');
+            zoomed = true;
+        }
+    });
+}
